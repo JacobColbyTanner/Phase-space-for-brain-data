5681c79eda47d94881a2717e008f88c61a07aa9d
Jacob C Tanner <jacobcolbytanner@gmail.com>
2023-12-08 15:47:19 -0500

    update

---
+++ CTRNN.py
@@ -0,0 +1,92 @@
+
+# Define networks
+import torch
+import torch.nn as nn
+from torch.nn import init
+from torch.nn import functional as F
+import math
+    
+
+class CTRNN(nn.Module):
+    """Continuous-time RNN.
+
+    Parameters:
+        input_size: Number of input neurons
+        hidden_size: Number of hidden neurons
+        dt: discretization time step in ms. 
+            If None, dt equals time constant tau
+
+    Inputs:
+        input: tensor of shape (seq_len, batch, input_size)
+        hidden: tensor of shape (batch, hidden_size), initial hidden activity
+            if None, hidden is initialized through self.init_hidden()
+        
+    Outputs:
+        output: tensor of shape (seq_len, batch, hidden_size)
+        hidden: tensor of shape (batch, hidden_size), final hidden activity
+    """
+
+    def __init__(self, input_size, hidden_size, dt=None, **kwargs):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.tau = 100
+        if dt is None:
+            alpha = 1
+        else:
+            alpha = dt / self.tau
+        self.alpha = alpha
+
+        self.input2h = nn.Linear(input_size, hidden_size)
+        self.h2h = nn.Linear(hidden_size, hidden_size)
+
+    def init_hidden(self, input_shape):
+        batch_size = input_shape[1]
+        return torch.zeros(batch_size, self.hidden_size)
+
+    def recurrence(self, input, hidden):
+        """Run network for one time step.
+        
+        Inputs:
+            input: tensor of shape (batch, input_size)
+            hidden: tensor of shape (batch, hidden_size)
+        
+        Outputs:
+            h_new: tensor of shape (batch, hidden_size),
+                network activity at the next time step
+        """
+        h_new = torch.relu(self.input2h(input) + self.h2h(hidden))
+        h_new = hidden * (1 - self.alpha) + h_new * self.alpha
+        return h_new
+
+    def forward(self, input, hidden=None):
+        """Propogate input through the network."""
+        
+        # If hidden activity is not provided, initialize it
+        if hidden is None:
+            hidden = self.init_hidden(input.shape).to(input.device)
+
+        # Loop through time
+        output = []
+        input_projection = []
+        steps = range(input.size(0))
+        for i in steps:
+            hidden = self.recurrence(input[i], hidden)
+            output.append(hidden)
+            input_projection.append(self.input2h(input[i]))
+
+        # Stack together output from all time steps
+        output = torch.stack(output, dim=0)  # (seq_len, batch, hidden_size)
+        #input_projection = torch.stack(input_projection, dim=0)  
+        
+        return output, hidden
+
+
+
+    
+    
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,108 @@
 
+from scipy.io import loadmat, savemat
+import numpy as np
+import model as net
+from torch.utils.data import TensorDataset, DataLoader
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import time
+from scipy.io import loadmat
+import CTRNN
+
+# Hyperparameters
+
+batch_size = 25
+sequence_length = 5
+num_iter = 10
+learning_rate = 0.000001
+epochs = 1000
+input_size = 10
+hidden_size = 200
+
+#Add model
+dt = 100
+net = CTRNN.CTRNN(input_size, hidden_size, dt=dt)
+
+
+
+# open data
+data = loadmat('/Users/jacobtanner/Brain networks lab Dropbox/Jacob Tanner/jacobcolbytanner/schaefer200_HCP7t_movie_rest_struct.mat')
+brain_data = data['HCP_7t_movie_rest']
+
+
+
+def get_batch(brain_data, sequence_length, batch_size,hidden_size):
+  
+
+    subject = np.random.randint(0,high=129)
+    scan = np.random.randint(0,high=3)
+    
+    TT = brain_data[0,subject]
+    ts = torch.from_numpy(TT['rest'][0,scan][0])  #time by nodes
+    
+
+
+    inputs = torch.zeros(sequence_length,batch_size,hidden_size)
+    
+    for i in range(batch_size):
+        start = np.random.randint(0,high= ts.shape[0]-sequence_length)
+        stop = start+sequence_length
+        inputs[:,i,:] = ts[start:stop,:]
+        
+
+    return inputs
+
+
+
+
+
+
+# Loss function
+criterion = nn.MSELoss()  
+
+# Optimizer
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# Training Loop
+for epoch in range(epochs):
+    model.train()  # Set the model to training mode
+    total_loss = 0
+    start_time = time.time()
+    r = []
+    for j in range(num_iter):
+        #create batches
+        real_hidden = get_batch(brain_data, sequence_length, batch_size,ntokens,out_size)
+        
+        #network inputs and starting state
+        hidden_starting_states = torch.from_numpy(real_hidden[0,:,:].squeeze())
+        inputs = torch.randn(sequence_length,batch_size,input_size)
+        # Forward pass
+        outputs, hidden = net(inputs,hidden_starting_states)
+     
+        loss = criterion(outputs, real_hidden)
+
+        O = outputs.detach().numpy().squeeze()
+        T = targets.detach().numpy().squeeze()
+
+        corr = np.corrcoef(O,T)
+        r.append(corr[0,1])
+        # Backward pass and optimization
+        optimizer.zero_grad()  # Clear old gradients
+        loss.backward()        # Compute gradients
+        optimizer.step()       # Update weights
+
+        total_loss += loss.item()
+    #plt.plot(outputs[-1,0,:].detach().numpy())
+    #plt.plot(targets[-1,0,:].detach().numpy())
+    #plt.show()
+
+    
+
+    end_time = time.time()-start_time
+    # Print average loss for the epoch
+    print("Epoch: ", epoch," Loss: ", total_loss/num_iter,"r: ",np.nanmean(r), "time: ", end_time)
+
+
+torch.save(model.state_dict(), 'models/CTRNN_fMRI.pth')
